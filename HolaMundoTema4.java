40363223344308fadaf6416b0dc906b27fdb56af
Francisco Mendoza Sánchez <paco@localhost.localdomain>
2017-03-31 18:11:07 +0200

    Por fin tengo JDK en Debian!

--- HolaMundoTema4.java
+++ HolaMundoTema4.java
@@ -1,1 +1,16 @@
-sdf
+//Mi primer programa JAVA HolaMundTema4.java
+/**
+Esta clase consutrye un programa que muestra en pantalla un mensaje simple
+*/
+/*También muestra informacioń sobre el nombre y la edad de una persona*/
+import java.io.*;
+public class HolaMundoTema4
+
+{
+public static void main (String[] args)
+{
+int edad = 19;
+System.out.println("Hola mundo");;
+}
+
+}
